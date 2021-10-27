db173835fee551eb572380de35a178c13cd50ae6
Vulpe-Yu <83893298+Vulpe-Yu@users.noreply.github.com>
2021-10-27 11:24:09 +0800

    Add files via upload

---
+++ CW5plus/CW5plus/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿
+namespace CW5plus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CreateBoard = new System.Windows.Forms.Button();
+            this.YInt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.XInt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Col = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Row = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Y3 = new System.Windows.Forms.TextBox();
+            this.Y2 = new System.Windows.Forms.TextBox();
+            this.Y1 = new System.Windows.Forms.TextBox();
+            this.X3 = new System.Windows.Forms.TextBox();
+            this.X2 = new System.Windows.Forms.TextBox();
+            this.X1 = new System.Windows.Forms.TextBox();
+            this.Col3 = new System.Windows.Forms.TextBox();
+            this.Col2 = new System.Windows.Forms.TextBox();
+            this.Row3 = new System.Windows.Forms.TextBox();
+            this.Row2 = new System.Windows.Forms.TextBox();
+            this.Col1 = new System.Windows.Forms.TextBox();
+            this.Row1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CreateBoard);
+            this.groupBox1.Controls.Add(this.YInt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.XInt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Col);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Row);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(490, 324);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PinBoard參數";
+            // 
+            // CreateBoard
+            // 
+            this.CreateBoard.Location = new System.Drawing.Point(298, 279);
+            this.CreateBoard.Name = "CreateBoard";
+            this.CreateBoard.Size = new System.Drawing.Size(173, 27);
+            this.CreateBoard.TabIndex = 4;
+            this.CreateBoard.Text = "Create Pin Board";
+            this.CreateBoard.UseVisualStyleBackColor = true;
+            this.CreateBoard.Click += new System.EventHandler(this.CreateBoard_Click);
+            // 
+            // YInt
+            // 
+            this.YInt.Location = new System.Drawing.Point(361, 185);
+            this.YInt.Name = "YInt";
+            this.YInt.Size = new System.Drawing.Size(100, 29);
+            this.YInt.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "列距";
+            // 
+            // XInt
+            // 
+            this.XInt.Location = new System.Drawing.Point(120, 185);
+            this.XInt.Name = "XInt";
+            this.XInt.Size = new System.Drawing.Size(100, 29);
+            this.XInt.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "行距";
+            // 
+            // Col
+            // 
+            this.Col.Location = new System.Drawing.Point(361, 89);
+            this.Col.Name = "Col";
+            this.Col.Size = new System.Drawing.Size(100, 29);
+            this.Col.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "直列數";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "橫行數";
+            // 
+            // Row
+            // 
+            this.Row.Location = new System.Drawing.Point(120, 89);
+            this.Row.Name = "Row";
+            this.Row.Size = new System.Drawing.Size(100, 29);
+            this.Row.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Calculate);
+            this.groupBox2.Controls.Add(this.Y3);
+            this.groupBox2.Controls.Add(this.Y2);
+            this.groupBox2.Controls.Add(this.Y1);
+            this.groupBox2.Controls.Add(this.X3);
+            this.groupBox2.Controls.Add(this.X2);
+            this.groupBox2.Controls.Add(this.X1);
+            this.groupBox2.Controls.Add(this.Col3);
+            this.groupBox2.Controls.Add(this.Col2);
+            this.groupBox2.Controls.Add(this.Row3);
+            this.groupBox2.Controls.Add(this.Row2);
+            this.groupBox2.Controls.Add(this.Col1);
+            this.groupBox2.Controls.Add(this.Row1);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 342);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(490, 364);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "各點參數";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(403, 317);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(68, 29);
+            this.Calculate.TabIndex = 15;
+            this.Calculate.Text = "計算";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Y3
+            // 
+            this.Y3.Location = new System.Drawing.Point(361, 251);
+            this.Y3.Name = "Y3";
+            this.Y3.ReadOnly = true;
+            this.Y3.Size = new System.Drawing.Size(74, 29);
+            this.Y3.TabIndex = 14;
+            // 
+            // Y2
+            // 
+            this.Y2.Location = new System.Drawing.Point(361, 182);
+            this.Y2.Name = "Y2";
+            this.Y2.ReadOnly = true;
+            this.Y2.Size = new System.Drawing.Size(74, 29);
+            this.Y2.TabIndex = 13;
+            // 
+            // Y1
+            // 
+            this.Y1.Location = new System.Drawing.Point(361, 114);
+            this.Y1.Name = "Y1";
+            this.Y1.ReadOnly = true;
+            this.Y1.Size = new System.Drawing.Size(74, 29);
+            this.Y1.TabIndex = 12;
+            // 
+            // X3
+            // 
+            this.X3.Location = new System.Drawing.Point(255, 251);
+            this.X3.Name = "X3";
+            this.X3.ReadOnly = true;
+            this.X3.Size = new System.Drawing.Size(74, 29);
+            this.X3.TabIndex = 11;
+            // 
+            // X2
+            // 
+            this.X2.Location = new System.Drawing.Point(255, 182);
+            this.X2.Name = "X2";
+            this.X2.ReadOnly = true;
+            this.X2.Size = new System.Drawing.Size(74, 29);
+            this.X2.TabIndex = 10;
+            // 
+            // X1
+            // 
+            this.X1.Location = new System.Drawing.Point(255, 114);
+            this.X1.Name = "X1";
+            this.X1.ReadOnly = true;
+            this.X1.Size = new System.Drawing.Size(74, 29);
+            this.X1.TabIndex = 4;
+            // 
+            // Col3
+            // 
+            this.Col3.Location = new System.Drawing.Point(120, 251);
+            this.Col3.Name = "Col3";
+            this.Col3.Size = new System.Drawing.Size(69, 29);
+            this.Col3.TabIndex = 9;
+            // 
+            // Col2
+            // 
+            this.Col2.Location = new System.Drawing.Point(120, 182);
+            this.Col2.Name = "Col2";
+            this.Col2.Size = new System.Drawing.Size(69, 29);
+            this.Col2.TabIndex = 8;
+            // 
+            // Row3
+            // 
+            this.Row3.Location = new System.Drawing.Point(21, 251);
+            this.Row3.Name = "Row3";
+            this.Row3.Size = new System.Drawing.Size(74, 29);
+            this.Row3.TabIndex = 7;
+            // 
+            // Row2
+            // 
+            this.Row2.Location = new System.Drawing.Point(21, 182);
+            this.Row2.Name = "Row2";
+            this.Row2.Size = new System.Drawing.Size(74, 29);
+            this.Row2.TabIndex = 6;
+            // 
+            // Col1
+            // 
+            this.Col1.Location = new System.Drawing.Point(120, 114);
+            this.Col1.Name = "Col1";
+            this.Col1.Size = new System.Drawing.Size(69, 29);
+            this.Col1.TabIndex = 5;
+            // 
+            // Row1
+            // 
+            this.Row1.Location = new System.Drawing.Point(21, 114);
+            this.Row1.Name = "Row1";
+            this.Row1.Size = new System.Drawing.Size(74, 29);
+            this.Row1.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(378, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 18);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Y值";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(276, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 18);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "X值";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(133, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 18);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "直列";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "橫行";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(518, 468);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Output.Size = new System.Drawing.Size(673, 185);
+            this.Output.TabIndex = 2;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(1078, 674);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(113, 32);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear Result";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(518, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(673, 396);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pin Board 畫面";
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(1051, 422);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(140, 31);
+            this.Reset.TabIndex = 0;
+            this.Reset.Text = "Reset Pin Board";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 718);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Row;
+        private System.Windows.Forms.TextBox YInt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox XInt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Col;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CreateBoard;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Col3;
+        private System.Windows.Forms.TextBox Col2;
+        private System.Windows.Forms.TextBox Row3;
+        private System.Windows.Forms.TextBox Row2;
+        private System.Windows.Forms.TextBox Col1;
+        private System.Windows.Forms.TextBox Row1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox Y3;
+        private System.Windows.Forms.TextBox Y2;
+        private System.Windows.Forms.TextBox Y1;
+        private System.Windows.Forms.TextBox X3;
+        private System.Windows.Forms.TextBox X2;
+        private System.Windows.Forms.TextBox X1;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ CW5plus/CW5plus/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _106_Quiz4; //using namespace。
+
+namespace CW5plus
+{
+    public partial class Form1 : Form
+    {
+        private RadioButton[,] radiobutton; //radiobutton陣列，RadioButton為單選，單一groupBox中僅可選中一個。
+        private GroupBox[,] groupbox; //groupbox陣列，搭配radiobutton陣列使用，使其可多選。
+        int counter = 0; //計算被選中的RadioButton數量。
+        RadioButton[] select = new RadioButton[3]; //被選中的RadioButton，陣列儲存。
+        private TextBox[,] rowcol = new TextBox[3,2]; //行列值TextBox陣列。
+        private TextBox[,] xyCoord = new TextBox[3, 2]; //XY值TextBox陣列。
+
+        public Form1() //建構子。
+        {
+            InitializeComponent(); //初始化物件。
+        }
+
+        private void CreateBoard_Click(object sender, EventArgs e) //Create Pin Board按鍵。
+        {
+            if (groupBox3.Controls.Count > 0) //groupBox3中控制元件總數大於0，即存在radiobutton。
+            {
+                foreach (Control item in radiobutton) //清除radiobutton陣列物件。
+                {
+                    item.Dispose();
+                }
+                foreach (Control item in groupbox) //清除groupbox陣列物件。
+                {
+                    item.Dispose();
+                }
+            }
+            int row = int.Parse(Row.Text);
+            int col = int.Parse(Col.Text); //讀入行列數。
+            radiobutton = new RadioButton[row, col]; //建立新radiobutton陣列。
+            groupbox = new GroupBox[row, col]; //建立新groupbox陣列。
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    this.radiobutton[i, j] = new RadioButton(); //建構新RadioButton物件。
+                    this.groupbox[i, j] = new GroupBox(); //建構新GroupBox物件。
+                    this.groupbox[i, j].Controls.Add(this.radiobutton[i, j]); //將RadioButton加到GroupBox中。(相當於綁定兩者。)
+                    this.groupbox[i, j].Size = new System.Drawing.Size(28, 34); //GroupBox長寬。
+                    this.radiobutton[i, j].Size = new System.Drawing.Size(16, 16); //RadioButton長寬。
+                    this.groupbox[i, j].Location = new System.Drawing.Point(20 + j * 34, 20 + i * 32); //新建GroupBox相對GroupBox3的位置，座標疊加。(參考點為左上角。)
+                    this.radiobutton[i, j].Location = new System.Drawing.Point(7, 12); //RadioButton相對GroupBox的位置。(參考點為左上角。)
+                    this.groupBox3.Controls.Add(this.groupbox[i, j]); //加入此GroupBox組件。
+                    this.radiobutton[i, j].Name = "radiobutton" + i.ToString() + j.ToString(); //對產生的RadioButton命名。
+                    this.radiobutton[i, j].CheckedChanged += new EventHandler(this.radiobutton_CheckedChanged);
+                }
+            }
+        }
+        
+        private void radiobutton_CheckedChanged(object sender, EventArgs e) //更改所選radiobutton。
+        {
+            select[counter] = (RadioButton)sender; //被選中的RadioButton。
+            if (select[counter].Checked == true) //該RadioButton被選中。
+            {
+                counter += 1; //計數器+1。
+            }
+            if (counter == 3) //有三個RadioButton被選中。
+            {
+                foreach (Control item in radiobutton) //鎖定所有RadioButton。
+                {
+                    item.Enabled = false;
+                }
+                foreach (Control item in select) //取消鎖定選中的RadioButton。
+                {
+                    item.Enabled = true;
+                }
+            }
+            int row = int.Parse(Row.Text);
+            int col = int.Parse(Col.Text); //讀入行列數。
+            rowcol[0, 0] = Row1;
+            rowcol[0, 1] = Col1;
+            rowcol[1, 0] = Row2;
+            rowcol[1, 1] = Col2;
+            rowcol[2, 0] = Row3;
+            rowcol[2, 1] = Col3; //TextBox陣列化。
+            xyCoord[0, 0] = X1;
+            xyCoord[0, 1] = Y1;
+            xyCoord[1, 0] = X2;
+            xyCoord[1, 1] = Y2;
+            xyCoord[2, 0] = X3;
+            xyCoord[2, 1] = Y3; //TextBox陣列化。
+            double xint = double.Parse(XInt.Text);
+            double yint = double.Parse(YInt.Text); //行列距取得。
+            for (int k = 0; k < counter; k++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        if (radiobutton[i, j] == select[k])
+                        {
+                            rowcol[k, 0].Text = i.ToString();
+                            rowcol[k, 1].Text = j.ToString(); //填入行列值。
+                            xyCoord[k, 0].Text = (j * xint).ToString();
+                            xyCoord[k, 1].Text = (i * yint).ToString(); //填入XY值。
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e) //Reset Pin Board按鍵。
+        {
+            counter = 0; //Reset Counter，需要放在第一行，否則有問題會影響到陣列索引值。
+            foreach (RadioButton item in radiobutton) //所有RadioButton。
+            {
+                item.Checked = false; //取消選取。
+                item.Enabled = true; //可被選取。
+            }
+            foreach (Control item in rowcol) //清空TextBox。
+            {
+                item.Text = "";
+                item.Enabled = true;
+            }
+            foreach (Control item in xyCoord)
+            {
+                item.Text = "";
+                item.Enabled = true;
+            }
+        }
+
+        private void Calculate_Click(object sender, EventArgs e) //計算按鍵。
+        {
+            string output = ""; //輸出訊息，初始為空。
+            Triangle triangle = new Triangle(); //建立new class Triangle。
+            triangle.CreatePoints(); //建構內部class Point。
+            triangle.ptArr[0].xCoord = double.Parse(X1.Text);
+            triangle.ptArr[0].yCoord = double.Parse(Y1.Text);
+            triangle.ptArr[1].xCoord = double.Parse(X2.Text);
+            triangle.ptArr[1].yCoord = double.Parse(Y2.Text);
+            triangle.ptArr[2].xCoord = double.Parse(X3.Text);
+            triangle.ptArr[2].yCoord = double.Parse(Y3.Text); //讀入當前座標值。
+            if (triangle.isValid() == true) //可形成三角形。
+            {
+                output += ("面積 = " + triangle.Area().ToString() + Environment.NewLine); //輸出面積。
+                //空字串基礎上加入輸出內容。
+                //Environment.NewLine換行。
+                output += ("周長 = " + triangle.Perimeter().ToString() + Environment.NewLine); //輸出周長。
+                switch (triangle.ShapeType()) //分辨類型。
+                {
+                    case 1:
+                        output += ("直角三角形" + Environment.NewLine + Environment.NewLine);
+                        break;
+                    case 2:
+                        output += ("銳角三角形" + Environment.NewLine + Environment.NewLine);
+                        break;
+                    case 3:
+                        output += ("鈍角三角形" + Environment.NewLine + Environment.NewLine);
+                        break;
+                }
+            }
+            else //無法形成三角形。
+            {
+                output += ("此三點無法形成三角形。" + Environment.NewLine + Environment.NewLine);
+            }
+            Output.AppendText(output); //輸出結果。
+            //或Output.Text = output;
+        }
+
+        private void Clear_Click(object sender, EventArgs e) //Clear Result按鍵。
+        {
+            Output.Clear(); //清空輸出方塊。
+        }
+    }
+}
